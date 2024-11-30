e4f47af6504d01a7deb37a397296412cb263bd8b
omrishoavi <omri.shoavi@gmail.com>
2024-11-30 14:56:15 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,43 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+
+public class Main {
+
+	    public static void main(String[] args) {
+	        int x = 1, y = 10; 
+	        Node<Integer> head = generateRandomNumbers(x, y);
+	        System.out.println("Generated List: " + head);
+
+	        int numberToCount = 5;
+	        int occurrences = countOccurrences(head, numberToCount);
+	        System.out.println("Number " + numberToCount + " appears " + occurrences + " times.");
+	    }
+
+	    
+	    public static Node<Integer> generateRandomNumbers(int x, int y) {
+	       
+	        Node<Integer> head = new Node<>((int) (Math.random() * (y - x + 1)) + x);
+	        Node<Integer> current = head;
+
+	        for (int i = 1; i < 20; i++) {
+	            Node<Integer> newNode = new Node<>((int) (Math.random() * (y - x + 1)) + x);
+	            current.setNext(newNode);
+	            current = newNode;
+	        }
+
+	        return head; 
+	    }
+
+	    
+	    public static int countOccurrences(Node<Integer> head, int target) {
+	        int count = 0;
+	        Node<Integer> current = head;
+
+	        while (current != null) {
+	            if (current.getValue() == target) {
+	                count++;
+	            }
+	            current = current.getNext();
+	        }
+
+	        return count;
+	    }
+	}
